3dd518b6c3e31032d9190ff5802535ba47b13253
dwikinugraha2359 <48194712+dwikinugraha2359@users.noreply.github.com>
2019-10-24 15:26:13 +0700

    Update run-jmeter.sh

--- run-jmeter.sh
+++ run-jmeter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 var1=$(docker inspect $(docker ps -a | grep -v "master" | awk 'NR>1 {print $1}' ) | grep -Po '"IPAddress":.*?[^\\]?[^\\]",' |awk '{print $2}'| sort -u | tr -d '"'| tr -d '\n')
 
+docker exec -it master /bin/bash -c "rm -rf $1 $2"
 docker cp /home/skeithnight/$1 master:$1
-echo ${var1::-1}
 docker exec -it master /bin/bash -c "jmeter -n -t /$1 -R${var1::-1} -l /$2"
 
 wait 
