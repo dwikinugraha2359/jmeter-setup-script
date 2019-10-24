2a3d12be3f85ccf53a98ffc912c2975d716ad927
dwikinugraha2359 <48194712+dwikinugraha2359@users.noreply.github.com>
2019-10-24 17:13:14 +0700

    Update run-jmeter.sh

--- run-jmeter.sh
+++ run-jmeter.sh
@@ -3,7 +3,7 @@ var1=$(docker inspect $(docker ps -a | grep -v "master" | awk 'NR>1 {print $1}' 
 
 docker exec -it master /bin/bash -c "rm -rf $1 $2"
 docker cp /home/skeithnight/$1 master:$1
-docker exec -it master /bin/bash -c "jmeter -n -t /$1 -R${var1::-1} -l /$2"
+docker exec -it master /bin/bash -c "jmeter -n -t /$1 -R${var1::-1} -l /$2 -e -o $3"
 
 wait 
 docker cp master:$2 /home/skeithnight/$2
